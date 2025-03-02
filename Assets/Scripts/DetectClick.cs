00a7a2a8ca0b387f29b89c39a06f3698afa1cf40
rookie-duck <evitaanggraini2910@gmail.com>
2025-03-02 11:53:03 +0700

    idk what to push

--- Assets/Scripts/DetectClick.cs
+++ Assets/Scripts/DetectClick.cs
@@ -1,18 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class DetectClick : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject tooltip; // The tooltip GameObject
+
+    [SerializeField]
+    private string directInteractorTag = "DirectInteractor"; // Tag for the hand or controller
+
+    private bool isTriggerPressed = false; // Current state of the trigger
+    private bool isInteractorInside = false; // Whether the interactor is inside this object
+
     void Start()
     {
-           
+        if (tooltip != null)
+        {
+            tooltip.SetActive(false); // Ensure the tooltip starts hidden
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        DetectTriggerPress();
+
+        // Toggle tooltip when the interactor is inside and trigger is pressed
+        if (isInteractorInside && isTriggerPressed)
+        {
+            ToggleTooltip();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the interacting object is the Direct Interactor
+        if (other.CompareTag(directInteractorTag))
+        {
+            isInteractorInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Reset the interactor state when it leaves the object
+        if (other.CompareTag(directInteractorTag))
+        {
+            isInteractorInside = false;
+        }
+    }
+
+    private void DetectTriggerPress()
+    {
+        var inputDevices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, inputDevices);
+
+        if (inputDevices.Count > 0)
+        {
+            InputDevice rightHandController = inputDevices[0];
+            bool triggerState;
+            rightHandController.TryGetFeatureValue(CommonUsages.triggerButton, out triggerState);
+
+            // Detect if the trigger is newly pressed
+            if (triggerState && !isTriggerPressed)
+            {
+                isTriggerPressed = true;
+            }
+            else if (!triggerState)
+            {
+                isTriggerPressed = false;
+            }
+        }
+    }
+
+    private void ToggleTooltip()
+    {
+        if (tooltip != null)
+        {
+            tooltip.SetActive(!tooltip.activeSelf); // Toggle tooltip visibility
+        }
     }
 }
